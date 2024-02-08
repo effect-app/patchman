1cc48774087fc96bd36c29ff4573d7c444dc94e5
Patrick Roza <contact@patrickroza.com>
2024-02-08 15:40:11 +0100

    cleanup

--- index.ts
+++ index.ts
@@ -1,48 +1,66 @@
-import { Option, ReadonlyArray, pipe } from "effect"
-const patches = (await import("./package.json", { assert: { type: "json"}})).default.pnpm.patchedDependencies
-
-import fs from "fs"
-import path from "path"
-
-const repoNames = process.argv[2]?.split(",") ?? []
-
-const repos = repoNames.map(repo => path.join(process.env.HOME!, "pj", repo))
-const desiredPatches = Object.entries(patches).map(([name, patch]) => {
-  return [name.substring(0, name.substring(1).indexOf("@") + 1), name, patch]
-})
+import { Option, ReadonlyArray, pipe } from "effect";
+import fs from "fs";
+import path from "path";
 
 const touch = async (filename: string) => {
   const time = new Date();
 
-await fs.promises.utimes(filename, time, time)
-.catch(async function (err) {
-    if ('ENOENT' !== err.code) {
-        throw err;
+  await fs.promises.utimes(filename, time, time).catch(async function (err) {
+    if ("ENOENT" !== err.code) {
+      throw err;
     }
-    let fh = await fs.promises.open(filename, 'a');
+    let fh = await fs.promises.open(filename, "a");
     await fh.close();
+  });
+};
+
+const repoNames = process.argv[2]?.split(",") ?? [];
+
+const patches = (await import("./package.json", { assert: { type: "json" } }))
+  .default.pnpm.patchedDependencies;
+
+const repos = repoNames.map((repo) => path.join(process.env.HOME!, "pj", repo));
+const desiredPatches = Object.entries(patches).map(([name, patch]) => {
+  return [name.substring(0, name.substring(1).indexOf("@") + 1), name, patch];
 });
-}
 
-await Promise.all(repos.map(async repo => {
-  const pj = (await import(path.join(repo, "package.json"), { assert: { type: "json"}})).default
+await Promise.all(
+  repos.map(async (repo) => {
+    const pj = await import(path.join(repo, "package.json"), {
+      assert: { type: "json" },
+    }).then((_) => _.default);
 
-  const patchies = pipe(Object.entries(pj.pnpm.patchedDependencies).map(([name, patch]) => {
-    return name.substring(0, name.substring(1).indexOf("@") + 1)
-  }), ReadonlyArray.filterMap(name => {
-    const match = desiredPatches.find(([desiredName, desiredPatch]) => desiredName === name)
-    return match ? Option.some(match) : Option.none()
-  }))
+    const patchies = pipe(
+      Object.entries(pj.pnpm.patchedDependencies).map(([name, patch]) => {
+        return name.substring(0, name.substring(1).indexOf("@") + 1);
+      }),
+      ReadonlyArray.filterMap((name) => {
+        const match = desiredPatches.find(
+          ([desiredName, desiredPatch]) => desiredName === name
+        );
+        return match ? Option.some(match) : Option.none();
+      })
+    );
 
-  const repoPatches = path.join(repo, "patches")
-  fs.rmSync(repoPatches, { recursive: true })
-  fs.mkdirSync(repoPatches)
-  await touch(repoPatches + "/.gitkeep")
+    const repoPatches = path.join(repo, "patches");
+    fs.rmSync(repoPatches, { recursive: true });
+    fs.mkdirSync(repoPatches);
+    await touch(repoPatches + "/.gitkeep");
 
-  pj.pnpm.patchedDependencies = patchies.reduce((acc, [name, desiredName, desiredPatch]) => { acc[desiredName]= desiredPatch; return acc}, { } as any)
-  fs.writeFileSync(path.join(repo, "package.json"), JSON.stringify(pj, null, 2))
+    pj.pnpm.patchedDependencies = patchies.reduce(
+      (acc, [name, desiredName, desiredPatch]) => {
+        acc[desiredName] = desiredPatch;
+        return acc;
+      },
+      {} as any
+    );
+    fs.writeFileSync(
+      path.join(repo, "package.json"),
+      JSON.stringify(pj, null, 2)
+    );
 
-  patchies.forEach(([name, desiredName, desiredPatch]) => {
-    fs.copyFileSync(desiredPatch, path.join(repo, desiredPatch))
+    patchies.forEach(([name, desiredName, desiredPatch]) => {
+      fs.copyFileSync(desiredPatch, path.join(repo, desiredPatch));
+    });
   })
-}))
+);
