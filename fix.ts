e49b3b117245aaf467332a91f8fc9050dd9abfca
Patrick Roza <contact@patrickroza.com>
2024-03-20 19:02:26 +0100

    bs

--- fix.ts
+++ fix.ts
@@ -7,9 +7,24 @@ const effectPJ = `{
 }
 `;
 
+const bs = `"sideEffects": [
+  "./dist/esm/fluentExtensions.js",
+  "./dist/esm/index.js",
+  "./dist/cjs/fluentExtensions.js",
+  "./dist/cjs/index.js"
+],`
+
 import fs from "fs";
 fs.writeFileSync(
   "/Users/patrickroza/pj/effect/effect/packages/effect/dist/dist/esm/package.json",
   effectPJ,
   { encoding: "utf-8" }
 );
+
+fs.writeFileSync(
+  "/Users/patrickroza/pj/effect/effect/packages/effect/dist/package.json", 
+  fs.readFileSync(
+    "/Users/patrickroza/pj/effect/effect/packages/effect/dist/package.json", "utf-8")
+    .replace(`"sideEffects": [],`, bs), 
+  "utf-8"
+)
--- patch.sh
+++ patch.sh
@@ -3,7 +3,7 @@
 pnpm tsx ./fix.ts
 
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
-npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
-npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
+# npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
+# npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
 
-~/pj/patchman.sh
+#~/pj/patchman.sh
