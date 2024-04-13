768db3944cea8fa5ce10452b04c4f5f611ea984a
Patrick Roza <contact@patrickroza.com>
2024-04-13 13:30:13 +0200

    platform-node patch

--- patch.sh
+++ patch.sh
@@ -9,6 +9,7 @@ cd packages/schema && pnpm build
 cd ../effect && pnpm build
 #cd ../opentelemetry && pnpm build
 #cd ../platform && pnpm build
+cd ../platform-node && pnpm build
 
 cd $pwd
 
@@ -17,6 +18,7 @@ pnpm tsx ./fix.ts
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
 npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
 #npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
+npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
 #npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
 
 ~/pj/patchman.sh
