4fb6d887853f9476845ad6736c2f080ebd055be3
Patrick Roza <contact@patrickroza.com>
2024-05-22 09:44:49 +0200

    update packages

--- patch.sh
+++ patch.sh
@@ -8,7 +8,7 @@ pnpm clean
 cd packages/effect
 cd ../effect && pnpm build
 cd ../schema && pnpm build
-cd ../opentelemetry && pnpm build
+# cd ../opentelemetry && pnpm build
 cd ../platform && pnpm build
 # cd ../platform-node && pnpm build
 
@@ -16,7 +16,7 @@ cd $pwd
 
 pnpm tsx ./fix.ts
 
-npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
+# npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
 npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
 npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
