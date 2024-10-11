c931e5fe1fd80e357cd0b836f746e835e71c5c53
Patrick Roza <contact@patrickroza.com>
2024-10-11 08:48:30 +0200

    cleanup patches after jsdoc fix

--- patch.sh
+++ patch.sh
@@ -4,7 +4,9 @@
 
 pwd=$(pwd)
 cd ~/pj/effect/effect
-pnpm clean
+echo "cleaning, codemodding and lint fixing effect repo"
+pnpm clean && pnpm codemod && pnpm lint-fix
+echo "building effect packages"
 cd packages/effect
 cd ../effect && pnpm build
 cd ../schema && pnpm build
@@ -12,13 +14,13 @@ cd ../sql && pnpm build
 # cd ../opentelemetry && pnpm build
 cd ../platform && pnpm build
 # cd ../platform-node && pnpm build
+cd ../..
+git stash
 
 cd $pwd
 
-pnpm tsx ./fix.ts
-
 # npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
-#npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
+npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
 npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
 npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
 npx pnpm-patch-i -y @effect/sql ~/pj/effect/effect/packages/sql/dist
