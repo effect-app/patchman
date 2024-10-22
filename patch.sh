2e555b755777f470b8854f1d71d091dad77bb78d
Patrick Roza <contact@patrickroza.com>
2024-10-22 07:28:58 +0200

    update to latest effect without @effect/schema

--- patch.sh
+++ patch.sh
@@ -9,7 +9,7 @@ pnpm clean && pnpm codemod && pnpm lint-fix
 echo "building effect packages"
 cd packages/effect
 cd ../effect && pnpm build
-cd ../schema && pnpm build
+#cd ../schema && pnpm build
 cd ../sql && pnpm build
 # cd ../opentelemetry && pnpm build
 cd ../platform && pnpm build
@@ -21,7 +21,7 @@ cd $pwd
 
 # npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
-npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
+#npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
 npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
 npx pnpm-patch-i -y @effect/sql ~/pj/effect/effect/packages/sql/dist
 #npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
