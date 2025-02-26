0713f453546939a1a0f5ef1b9de9d6a0ed33e62c
Patrick Roza <contact@patrickroza.com>
2025-02-26 16:29:38 +0100

    patch rpc

--- patch.sh
+++ patch.sh
@@ -10,6 +10,7 @@ echo "building effect packages"
 cd packages/effect
 cd ../effect && pnpm build
 #cd ../schema && pnpm build
+cd ../rpc && pnpm build
 cd ../sql && pnpm build
 # cd ../opentelemetry && pnpm build
 cd ../platform && pnpm build
@@ -23,6 +24,7 @@ cd $pwd
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
 #npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
 npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
+npx pnpm-patch-i -y @effect/rpc ~/pj/effect/effect/packages/rpc/dist
 npx pnpm-patch-i -y @effect/sql ~/pj/effect/effect/packages/sql/dist
 #npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
 
