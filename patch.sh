55e4dbf953d0ef9825b67182271582ac84a120a3
Patrick Roza <contact@patrickroza.com>
2025-03-08 18:33:14 +0100

    experimental rpc

--- patch.sh
+++ patch.sh
@@ -14,6 +14,10 @@ cd ../rpc && pnpm build
 cd ../sql && pnpm build
 # cd ../opentelemetry && pnpm build
 cd ../platform && pnpm build
+cd ../platform-browser && pnpm build
+cd ../platform-node && pnpm build
+cd ../platform-node-shared && pnpm build
+cd ../experimental && pnpm build
 #cd ../platform-node && pnpm build
 cd ../..
 git stash
@@ -22,10 +26,12 @@ cd $pwd
 
 # npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
 npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
-#npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
+npx pnpm-patch-i -y @effect/experimental ~/pj/effect/effect/packages/experimental/dist
 npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
+npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
+npx pnpm-patch-i -y @effect/platform-node-shared ~/pj/effect/effect/packages/platform-node-shared/dist
+npx pnpm-patch-i -y @effect/platform-browser ~/pj/effect/effect/packages/platform-browser/dist
 npx pnpm-patch-i -y @effect/rpc ~/pj/effect/effect/packages/rpc/dist
 npx pnpm-patch-i -y @effect/sql ~/pj/effect/effect/packages/sql/dist
-#npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
 
-~/pj/patchman.sh
+~/pj/patchman.exp.sh
