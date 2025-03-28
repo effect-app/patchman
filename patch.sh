7f9be4adcf4a323c3c492cbb08f7e70f0d941401
Patrick Roza <contact@patrickroza.com>
2025-03-28 07:24:11 +0100

    improve patch script

--- patch.sh
+++ patch.sh
@@ -3,29 +3,30 @@
 # TODO: iterate instead: schema, effect, opentelemetry, platform
 
 pwd=$(pwd)
+
 cd ~/pj/effect/effect
 echo "cleaning, codemodding and lint fixing effect repo"
 pnpm clean && pnpm codemod && pnpm lint-fix
-echo "building effect packages"
-cd packages/effect
-cd ../effect && pnpm build
-#cd ../schema && pnpm build
-cd ../rpc && pnpm build
-cd ../sql && pnpm build
-# cd ../opentelemetry && pnpm build
-cd ../platform && pnpm build
-#cd ../platform-node && pnpm build
-cd ../..
+echo "building and patching effect packages"
+
+declare -a arr=(
+  "effect"
+#  "rpc"
+  "sql"
+  "platform"
+#  "platform-node"
+  "opentelemetry"
+)
+
+for i in "${arr[@]}"
+do
+  cd ~/pj/effect/effect/packages/${i}
+  pnpm build
+  cd $pwd
+  npx pnpm-patch-i -y $i ~/pj/effect/effect/packages/${i}/dist
+done
+
+cd ~/pj/effect/effect
 git stash
-
-cd $pwd
-
-# npx pnpm-patch-i -y @effect/opentelemetry ~/pj/effect/effect/packages/opentelemetry/dist
-npx pnpm-patch-i -y effect ~/pj/effect/effect/packages/effect/dist
-#npx pnpm-patch-i -y @effect/schema ~/pj/effect/effect/packages/schema/dist
-npx pnpm-patch-i -y @effect/platform ~/pj/effect/effect/packages/platform/dist
-npx pnpm-patch-i -y @effect/rpc ~/pj/effect/effect/packages/rpc/dist
-npx pnpm-patch-i -y @effect/sql ~/pj/effect/effect/packages/sql/dist
-#npx pnpm-patch-i -y @effect/platform-node ~/pj/effect/effect/packages/platform-node/dist
 
 ~/pj/patchman.sh
